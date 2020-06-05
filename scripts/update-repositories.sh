e2d6786f26ded2ccf55997fb0157ceec6c04fbe1
michael <michael.chin@sydney.edu.au>
2020-06-05 16:03:12 +1000

    update script

--- scripts/update-repositories.sh
+++ scripts/update-repositories.sh
@@ -1,1 +1,14 @@
-echo `ls -l`
+#!/bin/bash
+
+git config --global user.name "michaelchin"
+git config --global user.email "michael.chin@sydney.edu.au"
+
+cp -rf ./private/mysite/* ./public/mysite/*
+
+cd ./public
+
+git add -A
+git commit --message "test commit"
+git push origin
+
+echo "Done!"
