2d68e72596a8d210846431159aa7099582d0bf4b
michael <michael.chin@sydney.edu.au>
2020-06-05 19:43:29 +1000

    update workflow

--- scripts/update-repositories.sh
+++ scripts/update-repositories.sh
@@ -3,6 +3,10 @@
 git config --global user.name "michaelchin"
 git config --global user.email "michael.chin@sydney.edu.au"
 
+cd private
+jekyll build
+cd ..
+
 cp -rf ./private/_site/* ./public/static/
 
 cd ./public
