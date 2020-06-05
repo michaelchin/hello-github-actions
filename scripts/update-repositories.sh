56aa0782addeceff911e0beb8b3b5be52e03cf5e
michael <michael.chin@sydney.edu.au>
2020-06-05 20:00:56 +1000

    update workflow

--- scripts/update-repositories.sh
+++ scripts/update-repositories.sh
@@ -4,6 +4,8 @@ git config --global user.name "michaelchin"
 git config --global user.email "michael.chin@sydney.edu.au"
 
 cd private
+touch Gemfile.lock
+chmod a+w Gemfile.lock
 jekyll build
 cd ..
 
